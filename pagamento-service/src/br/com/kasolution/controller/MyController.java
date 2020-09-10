094f38381f15d05be45e159d2165549d1776f86d
igorthribeiro <igorth.genesis@gmail.com>
2020-09-10 16:35:03 -0300

    correção do nome do método

--- pagamento-service/src/br/com/kasolution/controller/MyController.java
+++ pagamento-service/src/br/com/kasolution/controller/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyController {
 	
 	@RequestMapping("/")
-	public String ola() {
+	public String index() {
 		return "index";	
 	}
 	
